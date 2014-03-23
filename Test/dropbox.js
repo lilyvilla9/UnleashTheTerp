185a806208176897e577e8b8d755b83b9f4f6d93
Liliana Orellana <lorella9@terpmail.umd.edu>
2014-03-23 01:57:54 -0400

    Adding test files for OrganizeDrop extension

---
+++ Test/dropbox.js
@@ -0,0 +1,39 @@
+var client = new Dropbox.Client({key: "jenqtwz6nhaux4x"});
+
+// Try to finish OAuth authorization.
+client.authenticate({interactive: false}, function (error) {
+    if (error) {
+        alert('Authentication error: ' + error);
+    }
+});
+
+if (client.isAuthenticated()) {
+    // Client is authenticated
+    document.write("<script>alert('hello world')</script>");
+}
+
+/*
+var datastoreManager = client.getDatastoreManager();
+datastoreManager.openDefaultDatastore(function (error, datastore) {
+    if (error) {
+        alert('Error opening default datastore: ' + error);
+    }
+
+    var taskTable = datastore.getTable('tasks');
+
+    var firstTask = taskTable.insert({
+        taskname: 'Buy milk',
+        completed: false,
+        created: new Date()
+    });
+
+    var taskname = firstTask.get('taskname');
+    firstTask.set('completed', true);
+    firstTask.deleteRecord();
+
+    var results = taskTable.query();
+
+    var results = taskTable.query({completed: false});
+    var firstResult = results[0];
+});
+*/
---
+++ Test/save.js
@@ -0,0 +1,22 @@
+var rightClick = chrome.contextMenus.create({
+  "title" : "Save using LiliDrop",
+  "contexts" : ["image","link"],
+  "onclick": myClick
+});
+
+function myClick(info, tab) {
+    alert("do something");
+    var client = new Dropbox.Client({key: "jenqtwz6nhaux4x"});
+
+    // Try to finish OAuth authorization.
+    client.authenticate({interactive: false}, function (error) {
+        if (error) {
+            alert('Authentication error: ' + error);
+        }
+    });
+
+    if (client.isAuthenticated()) {
+        // Client is authenticated
+        alert("hello");
+    }
+}
