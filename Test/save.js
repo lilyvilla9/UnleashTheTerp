ad22702d770b1d488b374ceb7194001374e48bfb
Liliana Orellana <lorella9@terpmail.umd.edu>
2014-03-23 04:06:34 -0400

    Added commands to create a database (untested).

--- Test/save.js
+++ Test/save.js
@@ -1,13 +1,15 @@
 var rightClick = chrome.contextMenus.create({
   "title" : "Save using LiliDrop",
   "contexts" : ["image","link"],
-  "onclick": myClick
+  "onclick": authen
 });
 
-function myClick(info, tab) {
+var client;
+
+function authen(info, tab) {
     alert("start");
 
-    var client = new Dropbox.Client({key: "jenqtwz6nhaux4x"});
+    client = new Dropbox.Client({key: "jenqtwz6nhaux4x"});
 
     // Try to finish OAuth authorization
     client.authenticate({interactive: true}, function (error) {
@@ -18,8 +20,46 @@ function myClick(info, tab) {
 
     if (client.isAuthenticated()) {
         // Client is authenticated
-        alert("hello");
+        alert("start2");
+
+        saveFile(info);
+
+        alert("end2");
     }
 
     alert("end");
+}
+
+function saveFile(info) {
+    alert(info);
+
+    var datastoreManager = client.getDatastoreManager();
+
+    datastoreManager.openDefaultDatastore(function (error, datastore) {
+        if (error) {
+            alert("Error opening default datastore: " + error);
+        }
+
+        var defaultsTable = datastore.getTable("defaults");
+
+        defaultsTable.insert({ audio : "audio" });
+        defaultsTable.insert({ documents : "documents" });
+        defaultsTable.insert({ images : "images" });
+        defaultsTable.insert({ pdfs : "pdfs" });
+        defaultsTable.insert({ videos : "videos" });
+
+        client.mkdir("audio", defaultMkdir);
+        client.mkdir("documents", defaultMkdir);
+        client.mkdir("images", defaultMkdir);
+        client.mkdir("pdfs", defaultMkdir);
+        client.mkdir("videos", defaultMkdir);
+
+        var pathTable = datastore.getTable("paths");
+    });
+}
+
+function defaultMkdir(error, stat) {
+    if (error) {
+        alert("Error making default folder: " + error);
+    }
 }
